87318d064b566fc4c9091b77d63ecc7e92a13357
Ephraim Agumbada <ephraimduchis@gmail.com>
2025-10-28 22:39:14 +0100

    HIS

--- src/main/kotlin/com/example/data/database/DbConfig.kt
+++ src/main/kotlin/com/example/data/database/DbConfig.kt
@@ -8,7 +8,7 @@ import org.jetbrains.exposed.sql.Database
 fun Application.configureDatabases() {
 
     Database.connect(
-        url = "jdbc:postgresql://dpg-cqg4s78gph6c73ai1mlg-a.oregon-postgres.render.com:5432/futo_his_pgdb",
+        url = "jdbc:postgresql://dpg-cqg4s78gph6c73ai1mlg-a:5432/futo_his_pgdb",
         driver = "org.postgresql.Driver",
         user = "futo_his_pgdb_user",
         password = "futo_his_pgdb_user"
